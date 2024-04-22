5ab2067211d36161cc73f76dca92f01aefb76940
Phoomparin Mano <phoomparin@gmail.com>
2024-04-22 23:26:34 +0700

    use JWT as default authentication scheme

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -3,7 +3,6 @@ import { MetabaseProvider } from "@metabase/embedding-sdk-react"
 import {
   AUTH_API_HOST,
   JWT_PROVIDER_URI,
-  METABASE_API_KEY,
   METABASE_INSTANCE_URL,
 } from "../constants/env"
 
@@ -11,15 +10,18 @@ interface Props {
   children: React.ReactNode
 }
 
+/**
+ * Configuration for the Metabase provider.
+ *
+ * To authenticate with an API key instead of JWT, you can use the following configuration instead:
+ * { authType: "apiKey", apiKey: METABASE_API_KEY }
+ */
 const config = {
   font: "Lato",
   metabaseInstanceUrl: METABASE_INSTANCE_URL,
 
-  authType: "apiKey",
-  apiKey: METABASE_API_KEY,
-
-  // authType: "jwt",
-  // jwtProviderUri: `${AUTH_API_HOST}${JWT_PROVIDER_URI}`,
+  authType: "jwt",
+  jwtProviderUri: `${AUTH_API_HOST}${JWT_PROVIDER_URI}`,
 }
 
 export const AppProvider = ({ children }: Props) => {
