58404edf3d9bb849587bc9fd61559c842d520b80
Phoomparin Mano <phoomparin@gmail.com>
2024-04-24 15:19:19 +0700

    link back to /admin/products on logo click

--- src/components/layout/Shell.tsx
+++ src/components/layout/Shell.tsx
@@ -2,6 +2,7 @@ import { AppShell, Text } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
 import { ReactNode } from "react"
 import { SidebarLinks } from "./SidebarLinks"
+import { Link } from "wouter"
 
 interface Props {
   children: ReactNode
@@ -20,9 +21,11 @@ export function Shell(props: Props) {
       padding="md"
     >
       <AppShell.Navbar p="md" bg="transparent" withBorder={false}>
-        <Text fw={900} size="lg" c="#98D9D9" lh="xs" lts={2}>
-          CUSTOMER <br /> ZERO
-        </Text>
+        <Link to="/admin/products">
+          <Text fw={900} size="lg" c="#98D9D9" lh="xs" lts={2}>
+            CUSTOMER <br /> ZERO
+          </Text>
+        </Link>
 
         <SidebarLinks />
       </AppShell.Navbar>
