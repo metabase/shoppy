4b0f36b3104c958fd97006f30ea042d806ef7711
Phoomparin Mano <phoomparin@gmail.com>
2024-04-30 23:36:20 +0700

    add basic theming example

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -23,10 +23,45 @@ const config = {
 
 export const queryClient = new QueryClient()
 
+type _TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
+  ? R
+  : _TupleOf<T, N, [T, ...R]>
+
+export type Tuple<T, N extends number> = N extends N
+  ? number extends N
+    ? T[]
+    : _TupleOf<T, N, []>
+  : never
+
+export const colorTuple = (v: string): Tuple<string, 10> => [
+  v,
+  v,
+  v,
+  v,
+  v,
+  v,
+  v,
+  v,
+  v,
+  v,
+]
+
+const theme = {
+  fontFamily: "Lato, sans-serif",
+  headings: { fontFamily: "Lato, sans-serif" },
+  colors: {
+    brand: colorTuple("hotpink"),
+    "text-dark": colorTuple("hotpink"),
+    "text-light": colorTuple("hotpink"),
+  },
+}
+
 export const AppProvider = ({ children }: Props) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <MetabaseProvider config={config}>{children}</MetabaseProvider>
+      <MetabaseProvider config={config} theme={theme}>
+        {children}
+      </MetabaseProvider>
     </QueryClientProvider>
   )
 }
