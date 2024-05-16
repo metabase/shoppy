7afd72ee95dfdb430267a65c29d6336f0dc9cc76
Phoomparin Mano <phoomparin@gmail.com>
2024-05-16 18:21:30 +0700

    try to wrap in router

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, {Router} from "express"
 
 import { setupMiddleware } from "./middleware"
 import { restrict } from "./middleware/restrict"
@@ -13,16 +13,18 @@ import { PORT } from "./constants/env"
 const app = express()
 setupMiddleware(app)
 
-app.get("/", (_, res) => res.send({ status: "ready" }))
-app.post("/login", loginHandler)
-app.get("/sso/metabase", metabaseAuthHandler)
-app.get("/products", productListHandler)
-app.get("/product/:id", productDetailHandler)
-
-app.get("/user", restrict, (req: any, res: any) => {
+const router = Router()
+router.get("/", (_, res) => res.send({ status: "ok" }))
+router.post("/login", loginHandler)
+router.get("/sso/metabase", metabaseAuthHandler)
+router.get("/products", productListHandler)
+router.get("/product/:id", productDetailHandler)
+router.get("/user", restrict, (req: any, res: any) => {
   res.status(200).json({ user: req.session.user })
 })
 
+app.get("/", (_, res) => res.send({ status: "ok" }))
+app.use("/api", router)
 app.listen(PORT, () => {
   console.log(`[customer zero api] running at http://localhost:${PORT}`)
 })
