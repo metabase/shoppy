3650847cbdfc58a807ba5cd0eb10dd72db967e83
Phoomparin Mano <phoomparin@gmail.com>
2024-05-16 18:50:40 +0700

    relative api host

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -4,8 +4,7 @@
  *
  * See [api/src/main.ts] for the implementation used in this demo.
  */
-export const API_HOST =
-  process.env.REACT_APP_API_HOST ?? "http://localhost:3003"
+export const API_HOST = getAPIHost()
 
 export const JWT_PROVIDER_URI =
   process.env.REACT_APP_JWT_PROVIDER_URI ?? "/sso/metabase"
@@ -15,3 +14,15 @@ export const JWT_PROVIDER_URI =
  */
 export const METABASE_INSTANCE_URL =
   process.env.REACT_APP_METABASE_INSTANCE_URL ?? "http://localhost:3000"
+
+export function getAPIHost() {
+  const {
+    REACT_APP_API_HOST,
+    REACT_APP_VERCEL_ENV,
+  } = process.env
+
+  // See [https://vercel.com/docs/security/deployment-protection#migrating-to-standard-protection]
+  if (REACT_APP_VERCEL_ENV) return "/api"
+
+  return REACT_APP_API_HOST ?? "http://localhost:3003/api"
+}
