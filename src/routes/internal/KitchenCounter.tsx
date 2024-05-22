e8acc67e42b772063438bfaf3e161004a9f6ac7d
Oisin Coveney <oisin@metabase.com>
2024-05-22 18:50:52 +0300

    Add static dashboard code

---
+++ src/routes/internal/KitchenCounter.tsx
@@ -0,0 +1,76 @@
+import {Box, Title, Grid, Card, Stack, Flex} from "@mantine/core"
+import {StaticDashboard, StaticQuestion} from "@metabase/embedding-sdk-react"
+
+import "./kitchen-sink.css"
+
+const questions: number[] = [
+    // bar
+    95,
+
+    // line
+    96,
+
+    // area
+    97,
+]
+
+export const KitchenSink = () => (
+    <Stack>
+        <Title pb={8} size="h2">
+            Kitchen Sink
+        </Title>
+
+        <Grid pb={20}>
+            {questions.map((q) => (
+                <Grid.Col key={q} span={4}>
+                    <StaticQuestion questionId={q} showVisualizationSelector={false}/>
+                </Grid.Col>
+            ))}
+        </Grid>
+
+        <Title pb={8} size="h2">
+            Layout Test
+        </Title>
+
+        <Stack>
+            <Title pb={8} size="h4">
+                Flex
+            </Title>
+
+            <Flex>
+                <StaticQuestion questionId={95} showVisualizationSelector={false}/>
+            </Flex>
+
+            <Title pb={8} size="h4">
+                Card
+            </Title>
+
+            <Card w="100%">
+                <StaticQuestion questionId={95} showVisualizationSelector={false}/>
+            </Card>
+
+            <Title pb={8} size="h4">
+                Box
+            </Title>
+
+            <Box w="100%">
+                <StaticQuestion questionId={95} showVisualizationSelector={false}/>
+            </Box>
+
+            <Title pb={8} size="h4">
+                Table
+            </Title>
+
+            <Box w="100%" bg="white">
+                <StaticQuestion questionId={76} showVisualizationSelector={false}/>
+            </Box>
+
+            <Box>
+                <StaticDashboard dashboardId={11} parameterValues={{}} displayOptions={{
+                    theme: "night",
+                    titled: true,
+                }}/>
+            </Box>
+        </Stack>
+    </Stack>
+)
