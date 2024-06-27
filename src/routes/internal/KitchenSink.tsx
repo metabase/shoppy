bc76f0117d538b314176ec11bfd96c77cf88bc5b
Phoomparin Mano <phoomparin@gmail.com>
2024-06-27 14:55:38 +0700

    kitchen sink update

--- src/routes/internal/KitchenSink.tsx
+++ src/routes/internal/KitchenSink.tsx
@@ -1,18 +1,11 @@
-import { Box, Title, Grid, Card, Stack, Flex } from "@mantine/core"
-import { StaticQuestion } from "@metabase/embedding-sdk-react"
+import { Box, Title, Stack } from "@mantine/core"
+
+import {
+  InteractiveDashboard,
+  InteractiveQuestion,
+} from "@metabase/embedding-sdk-react"
 
 import "./kitchen-sink.css"
-
-const questions: number[] = [
-  // bar
-  95,
-
-  // line
-  96,
-
-  // area
-  97,
-]
 
 export const KitchenSink = () => (
   <Stack>
@@ -20,50 +13,11 @@ export const KitchenSink = () => (
       Kitchen Sink
     </Title>
 
-    <Grid pb={20}>
-      {questions.map((q) => (
-        <Grid.Col key={q} span={4}>
-          <StaticQuestion questionId={q} showVisualizationSelector={false} />
-        </Grid.Col>
-      ))}
-    </Grid>
-
-    <Title pb={8} size="h2">
-      Layout Test
-    </Title>
-
-    <Stack>
-      <Title pb={8} size="h4">
-        Flex
-      </Title>
-
-      <Flex>
-        <StaticQuestion questionId={95} showVisualizationSelector={false} />
-      </Flex>
-
-      <Title pb={8} size="h4">
-        Card
-      </Title>
-
-      <Card w="100%">
-        <StaticQuestion questionId={95} showVisualizationSelector={false} />
-      </Card>
-
-      <Title pb={8} size="h4">
-        Box
-      </Title>
-
-      <Box w="100%">
-        <StaticQuestion questionId={95} showVisualizationSelector={false} />
-      </Box>
-
-      <Title pb={8} size="h4">
-        Table
-      </Title>
-
-      <Box w="100%" bg="white">
-        <StaticQuestion questionId={76} showVisualizationSelector={false} />
-      </Box>
-    </Stack>
+    <Box>
+      <InteractiveQuestion questionId={95} height={500} />
+      <InteractiveQuestion questionId={95} height={500} />
+      <InteractiveDashboard dashboardId={17} />
+      <InteractiveDashboard dashboardId={18} />
+    </Box>
   </Stack>
 )
