7097cdaf229cd1ea981941f1c0c9d89aa9091c00
Phoomparin Mano <phoomparin@gmail.com>
2024-07-05 18:40:03 +0700

    get current site name from jotai

--- src/utils/current-site.ts
+++ src/utils/current-site.ts
@@ -1,3 +1,5 @@
+import { store } from "../store"
+import { siteAtom } from "../store/theme"
 import { SITE_CONFIG_MAP } from "../themes"
 
 import { SiteKey } from "../types/site"
@@ -5,7 +7,7 @@ import { SiteKey } from "../types/site"
 const THEME_NAMES = Object.keys(SITE_CONFIG_MAP)
 
 export function getCurrentSite(): SiteKey {
-  const site = JSON.parse(localStorage.getItem("site")!)
+  const site = store.get(siteAtom)
 
   if (!site || !THEME_NAMES.includes(site)) {
     return "stitch"
