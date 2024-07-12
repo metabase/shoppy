164416a7ef3670c1a22e35a9f7813fafd46b8fea
Phoomparin Mano <phoomparin@gmail.com>
2024-07-13 00:30:53 +0700

    test parameterized static questions

--- src/routes/internal/KitchenSink.tsx
+++ src/routes/internal/KitchenSink.tsx
@@ -1,24 +1,9 @@
-import { Title, Box, Stack, Flex } from "@mantine/core"
-import { InteractiveQuestion } from "@metabase/embedding-sdk-react"
+import { Title, Stack } from "@mantine/core"
 
 export const KitchenSink = () => (
   <Stack>
     <Title pb={8} size="h2">
       Kitchen Sink
     </Title>
-
-    <Stack className="gap-y-8">
-      <Flex className="overflow-y-scroll" h="700px">
-        <InteractiveQuestion questionId={144} height="100%" />
-      </Flex>
-
-      <Flex className="overflow-y-scroll" h="700px">
-        <InteractiveQuestion questionId={158} height="100%" />
-      </Flex>
-
-      <Flex className="overflow-y-scroll" h="700px">
-        <InteractiveQuestion questionId={160} height="100%" />
-      </Flex>
-    </Stack>
   </Stack>
 )
