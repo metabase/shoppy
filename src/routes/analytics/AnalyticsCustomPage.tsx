a51916d4df020d69e3223b5b53934737cf61f294
Phoomparin Mano <phoomparin@gmail.com>
2024-07-18 02:13:04 +0700

    improve collection browser style

--- src/routes/analytics/AnalyticsCustomPage.tsx
+++ src/routes/analytics/AnalyticsCustomPage.tsx
@@ -3,23 +3,26 @@ import { useLocation } from "wouter"
 import { CollectionBrowser } from "@metabase/embedding-sdk-react"
 
 import "./analytics-custom-page.css"
+import { RemountOnSiteChange } from "../../components/RemountOnSiteChange"
 
 export function AnalyticsCustomPage() {
   const [, navigate] = useLocation()
 
   return (
     <Box p={20}>
-      <CollectionBrowser
-        collectionId={0}
-        className="analytics-collection-browser"
-        onClick={(item) => {
-          if (item.model === "dashboard") {
-            navigate(`/analytics/${item.id}`)
-          } else if (item.model === "card") {
-            navigate(`/question/${item.id}`)
-          }
-        }}
-      />
+      <RemountOnSiteChange>
+        <CollectionBrowser
+          collectionId={0}
+          className="analytics-collection-browser"
+          onClick={(item) => {
+            if (item.model === "dashboard") {
+              navigate(`/analytics/${item.id}`)
+            } else if (item.model === "card") {
+              navigate(`/question/${item.id}`)
+            }
+          }}
+        />
+      </RemountOnSiteChange>
     </Box>
   )
 }
