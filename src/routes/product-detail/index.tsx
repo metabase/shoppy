118db753795ffe6fc7bb5386b0a9076757a00e1e
Phoomparin Mano <phoomparin@gmail.com>
2024-09-04 02:11:38 +0700

    prevent the user from seeing data from a different shop

--- src/routes/product-detail/index.tsx
+++ src/routes/product-detail/index.tsx
@@ -1,21 +1,26 @@
-import { Grid, Container, Box, Text, Flex } from "@mantine/core"
+import { useAtom } from "jotai"
+import { useEffect } from "react"
 import { useQuery } from "@tanstack/react-query"
+import { navigate } from "wouter/use-browser-location"
+import { Grid, Container, Box, Text, Flex } from "@mantine/core"
 
 import { ProductDetailCard } from "./ProductDetailCard"
 import { ProductDetailInsights } from "./ProductDetailInsights"
 
+import { siteAtom } from "../../store/site"
 import { truncate } from "../../utils/truncate"
-
 import { getProductById } from "../../utils/query-product"
 import { FullPageLoader } from "../../components/Loader"
-import { useSiteChanged } from "../../utils/use-site-changed"
-import { navigate } from "wouter/use-browser-location"
+
+import { SITES } from "../../constants/sites"
 
 interface Props {
   id: string
 }
 
 export const ProductDetailPage = ({ id }: Props) => {
+  const [siteKey] = useAtom(siteAtom)
+
   const query = useQuery({
     queryKey: ["product", id ?? null],
     queryFn: ({ queryKey: [_, id] }) => getProductById(parseInt(id, 10)),
@@ -23,8 +28,15 @@ export const ProductDetailPage = ({ id }: Props) => {
 
   const product = query.data
 
-  // When the site changed, navigate to the product listing page.
-  useSiteChanged(() => navigate("/admin/products"))
+  // If the product is from a different shop, redirect back to the product listing page.
+  // This prevents the user from seeing data from a different shop.
+  useEffect(() => {
+    const site = SITES.find((s) => s.key === siteKey)
+
+    if (product && site && site.shopId !== product.shopId) {
+      navigate("/admin/products")
+    }
+  }, [siteKey, product])
 
   if (query.isLoading) return <FullPageLoader />
   if (!product) return <div>Product not found</div>
