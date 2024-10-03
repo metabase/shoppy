8d3db571c1997a472ee3c0113e66037c149ff5da
Phoomparin Mano <phoomparin@gmail.com>
2024-10-04 03:15:01 +0700

    remove authcheck

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,73 +14,68 @@ import { NewDashboardPage } from "./analytics/new/NewDashboardPage"
 import { KitchenSink } from "./internal/KitchenSink"
 
 import { AppProvider } from "../components/AppProvider"
-import { AuthCheck } from "../components/AuthCheck"
 import { Shell } from "../components/layout/Shell"
 
 export const Routes = () => (
   <Switch>
-    <AuthCheck>
-      <Route path="/" component={() => <Redirect to="/admin/products" />} />
+    <Route path="/" component={() => <Redirect to="/admin/products" />} />
 
-      <AppProvider>
-        <Shell>
-          <Route path="/admin" nest>
-            <Switch>
-              <Route
-                path="/products"
-                component={() => <ProductAnalyticsPage />}
-              />
-
-              <Route
-                path="/categories/:id"
-                component={(props) => (
-                  <ProductAnalyticsPage categoryId={props.params.id} />
-                )}
-              />
-
-              <Route
-                path="/products/:id"
-                component={(props) => (
-                  <ProductDetailPage id={props.params.id} />
-                )}
-              />
-            </Switch>
-
-            <Switch>
-              <Route path="/analytics" component={AnalyticsOverviewPage} />
-              <Route path="/analytics/custom" component={AnalyticsCustomPage} />
-
-              <Route
-                path="/analytics/:id"
-                component={(props) => <DashboardPage id={props.params.id} />}
-              />
-
-              <Route
-                path="/analytics/new/from-template"
-                component={NewFromTemplatePage}
-              />
-
-              <Route
-                path="/analytics/new/from-scratch"
-                component={NewFromScratchPage}
-              />
-
-              <Route
-                path="/analytics/new/dashboard"
-                component={NewDashboardPage}
-              />
-            </Switch>
+    <AppProvider>
+      <Shell>
+        <Route path="/admin" nest>
+          <Switch>
+            <Route
+              path="/products"
+              component={() => <ProductAnalyticsPage />}
+            />
 
             <Route
-              path="/question/:id"
-              component={(props) => <QuestionPage id={props.params.id} />}
+              path="/categories/:id"
+              component={(props) => (
+                <ProductAnalyticsPage categoryId={props.params.id} />
+              )}
             />
-          </Route>
 
-          <Route path="/dev" component={KitchenSink} />
-        </Shell>
-      </AppProvider>
-    </AuthCheck>
+            <Route
+              path="/products/:id"
+              component={(props) => <ProductDetailPage id={props.params.id} />}
+            />
+          </Switch>
+
+          <Switch>
+            <Route path="/analytics" component={AnalyticsOverviewPage} />
+            <Route path="/analytics/custom" component={AnalyticsCustomPage} />
+
+            <Route
+              path="/analytics/:id"
+              component={(props) => <DashboardPage id={props.params.id} />}
+            />
+
+            <Route
+              path="/analytics/new/from-template"
+              component={NewFromTemplatePage}
+            />
+
+            <Route
+              path="/analytics/new/from-scratch"
+              component={NewFromScratchPage}
+            />
+
+            <Route
+              path="/analytics/new/dashboard"
+              component={NewDashboardPage}
+            />
+          </Switch>
+
+          <Route
+            path="/question/:id"
+            component={(props) => <QuestionPage id={props.params.id} />}
+          />
+        </Route>
+
+        <Route path="/dev" component={KitchenSink} />
+      </Shell>
+    </AppProvider>
 
     <Route>404: No such page!</Route>
   </Switch>
