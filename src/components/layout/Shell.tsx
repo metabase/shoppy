afe401bba8034afa946b57054e6aab40fb2fd494
Phoomparin Mano <phoomparin@gmail.com>
2024-10-08 02:40:41 +0700

    hide app shell header border

--- src/components/layout/Shell.tsx
+++ src/components/layout/Shell.tsx
@@ -29,11 +29,7 @@ export function Shell(props: Props) {
           navbar: "navbar overflow-scroll sm:overflow-visible",
         }}
       >
-        <AppShell.Header
-          zIndex={102}
-          bg="#2B2F32"
-          className="border-b-[#2B2F32]"
-        >
+        <AppShell.Header zIndex={102} bg="#2B2F32" className="border-none">
           <Flex
             justify="space-between"
             align="center"
