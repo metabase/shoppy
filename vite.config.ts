afa5355218530d752bb90e50e7dca50067eaafae
Phoomparin Mano <phoomparin@gmail.com>
2024-10-08 09:31:40 +0700

    use vite webfont plugin to avoid render blocking

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 import basicSsl from "@vitejs/plugin-basic-ssl"
+import webfontDownload from "vite-plugin-webfont-dl"
 
 const isHTTPS = process.env.HTTPS
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), isHTTPS ? basicSsl() : null],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    isHTTPS ? basicSsl() : null,
+    webfontDownload(),
+  ],
   server: {
     open: false,
     port: 3004,
