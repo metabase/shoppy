c54eb627d29da8c13a477b25da51514b3f0f1f46
Phoomparin Mano <phoomparin@gmail.com>
2024-10-08 09:46:41 +0700

    add notes on caching

--- api/src/utils/cache.ts
+++ api/src/utils/cache.ts
@@ -1,6 +1,8 @@
 import { Response } from "express"
 
-// cache for 1 week
+// cache for 1 week.
+// this cache-control header tells the browser to cache it for a week,
+// as well as tells Vercel's Edge CDN to cache via https://vercel.com/docs/edge-network/caching
 export const cacheStaticResponse = (res: Response) =>
   res.set(
     "Cache-Control",
