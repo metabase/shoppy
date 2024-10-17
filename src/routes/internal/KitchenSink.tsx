9889f40e90a1feff155d81d8117c713ac56a7402
Phoomparin Mano <phoomparin@gmail.com>
2024-10-18 06:58:06 +0700

    simple kitchen sink

--- src/routes/internal/KitchenSink.tsx
+++ src/routes/internal/KitchenSink.tsx
@@ -1,69 +1,20 @@
-import { Box, Title, Grid, Card, Stack, Flex } from "@mantine/core"
-import { StaticQuestion } from "@metabase/embedding-sdk-react"
+import { Stack, Title } from "@mantine/core"
+import { InteractiveQuestion } from "@metabase/embedding-sdk-react"
 
 import "./kitchen-sink.css"
 
-const questions: number[] = [
-  // bar
-  95,
-
-  // line
-  96,
-
-  // area
-  97,
-]
-
 export const KitchenSink = () => (
   <Stack>
-    <Title pb={8} size="h2">
-      Kitchen Sink
-    </Title>
-
-    <Grid pb={20}>
-      {questions.map((q) => (
-        <Grid.Col key={q} span={4}>
-          <StaticQuestion questionId={q} showVisualizationSelector={false} />
-        </Grid.Col>
-      ))}
-    </Grid>
-
-    <Title pb={8} size="h2">
-      Layout Test
-    </Title>
-
-    <Stack>
-      <Title pb={8} size="h4">
-        Flex
-      </Title>
-
-      <Flex>
-        <StaticQuestion questionId={95} showVisualizationSelector={false} />
-      </Flex>
-
-      <Title pb={8} size="h4">
-        Card
-      </Title>
-
-      <Card w="100%">
-        <StaticQuestion questionId={95} showVisualizationSelector={false} />
-      </Card>
-
-      <Title pb={8} size="h4">
-        Box
-      </Title>
-
-      <Box w="100%">
-        <StaticQuestion questionId={95} showVisualizationSelector={false} />
-      </Box>
-
-      <Title pb={8} size="h4">
-        Table
-      </Title>
-
-      <Box w="100%" bg="white">
-        <StaticQuestion questionId={76} showVisualizationSelector={false} />
-      </Box>
-    </Stack>
+    <InteractiveQuestion
+      questionId={158}
+      height={700}
+      withTitle
+      customTitle={
+        <Title fw={400} size="h2" className="product-detail-card-title">
+          Orders over time
+        </Title>
+      }
+      isSaveEnabled
+    />
   </Stack>
 )
