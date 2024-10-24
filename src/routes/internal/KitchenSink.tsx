0fce349d94b0ab2dc673278b118d6dea657ac4c1
Phoomparin Mano <phoomparin@gmail.com>
2024-10-24 18:44:59 +0700

    foo

--- src/routes/internal/KitchenSink.tsx
+++ src/routes/internal/KitchenSink.tsx
@@ -1,38 +1,10 @@
-import { Stack, Title } from "@mantine/core"
+import { Stack } from "@mantine/core"
 import { InteractiveQuestion } from "@metabase/embedding-sdk-react"
 
 import "./kitchen-sink.css"
 
 export const KitchenSink = () => (
   <Stack>
-    <InteractiveQuestion
-      questionId={158}
-      height={700}
-      withTitle
-      customTitle={
-        <Title fw={400} size="h2" className="product-detail-card-title">
-          Orders over time
-        </Title>
-      }
-      isSaveEnabled
-    />
-
-    <InteractiveQuestion
-      questionId={158}
-      height={700}
-      withTitle
-      customTitle={
-        <Title fw={400} size="h2" className="product-detail-card-title">
-          Orders over time
-        </Title>
-      }
-      isSaveEnabled={false}
-      onSave={(question) => {
-        console.log("InteractiveQuestion:onSave", { question })
-      }}
-      onBeforeSave={async (question) => {
-        console.log("InteractiveQuestion:onBeforeSave", { question })
-      }}
-    />
+    <InteractiveQuestion questionId={158} height={700} />
   </Stack>
 )
