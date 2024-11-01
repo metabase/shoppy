9489e4a8bee32c6cd1e7e2ac9a0f647a4fdeae5e
Phoomparin Mano <phoomparin@gmail.com>
2024-11-01 22:32:12 +0700

    add custom view components

--- src/components/InteractiveQuestionView.tsx
+++ src/components/InteractiveQuestionView.tsx
@@ -1,10 +1,47 @@
-import { Box } from "@mantine/core"
+import { Box, Flex, Group } from "@mantine/core"
 import { InteractiveQuestion } from "@metabase/embedding-sdk-react"
+
+import { ThemedButton } from "./ThemedButton"
+import { CustomIcon } from "./CustomIcon"
 
 export const InteractiveQuestionView = () => {
   return (
     <Box h="500px">
-      <Box>Foo Bar</Box>
+      <Flex w="100%" justify="space-between" pb={14}>
+        <Group gap="xs">
+          <InteractiveQuestion.BackButton />
+
+          <InteractiveQuestion.Title />
+        </Group>
+
+        <Group gap="xs">
+          <ThemedButton
+            size="compact-sm"
+            leftSection={<CustomIcon icon="filter" />}
+          >
+            Add a filter
+          </ThemedButton>
+
+          <ThemedButton
+            size="compact-sm"
+            leftSection={<CustomIcon icon="sum" />}
+          >
+            Change the summary
+          </ThemedButton>
+
+          <ThemedButton
+            size="compact-sm"
+            leftSection={<CustomIcon icon="sum" />}
+          >
+            Change the breakout
+          </ThemedButton>
+
+          <ThemedButton size="compact-sm">
+            <CustomIcon icon="notebook" />
+          </ThemedButton>
+        </Group>
+      </Flex>
+
       <InteractiveQuestion.QuestionVisualization />
     </Box>
   )
