509e2391eadfa2fe6f37b0fdd894d2e0e5855468
Phoomparin Mano <phoomparin@gmail.com>
2024-11-01 18:02:28 +0700

    add placeholder view

--- src/components/InteractiveQuestionView.tsx
+++ src/components/InteractiveQuestionView.tsx
@@ -1,3 +1,11 @@
+import { Box } from "@mantine/core"
+import { InteractiveQuestion } from "@metabase/embedding-sdk-react"
+
 export const InteractiveQuestionView = () => {
-  return <div>Empty Placeholder</div>
+  return (
+    <Box h="500px">
+      <Box>Foo Bar</Box>
+      <InteractiveQuestion.QuestionVisualization />
+    </Box>
+  )
 }
