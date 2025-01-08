0dfbe4a582793c52f591680349a77c6caedd7bf8
Phoomparin Mano <phoomparin@gmail.com>
2025-01-08 21:57:11 +0700

    auto save to sandboxed collection instead of showing a collection picker on proficiency

--- src/constants/collections.ts
+++ src/constants/collections.ts
@@ -6,10 +6,12 @@ export const SANDBOXED_CUSTOM_ANALYTICS_COLLECTIONS: Record<SiteKey, number> = {
   stitch: 45,
   luminara: 46,
   pug: 47,
+  proficiency: 100,
 }
 
 export const SANDBOXED_USER_GENERATED_COLLECTIONS: Record<SiteKey, number> = {
   stitch: 48,
   luminara: 49,
   pug: 50,
+  proficiency: 101,
 }
