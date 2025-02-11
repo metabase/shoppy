fc6654c0d50cd355a1a40bbff762caaa91d9bf48
Timofei Kachalov <timofei@metabase.com>
2025-02-12 00:03:00 +0400

    Read from .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 import basicSsl from "@vitejs/plugin-basic-ssl"
@@ -6,21 +6,25 @@ import webfontDownload from "vite-plugin-webfont-dl"
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 
 const isHTTPS = process.env.HTTPS
-const port = process.env.PORT ? parseInt(process.env.PORT) : undefined
 
-export default defineConfig({
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    isHTTPS ? basicSsl() : null,
-    webfontDownload(),
-    ViteImageOptimizer(),
-  ],
-  server: {
-    open: false,
-    port: 3004,
-  },
-  preview: {
-    port: port ?? 4173,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  const port = env.PORT ? parseInt(env.PORT) : undefined
+
+  return {
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      isHTTPS ? basicSsl() : null,
+      webfontDownload(),
+      ViteImageOptimizer(),
+    ],
+    server: {
+      open: false,
+      port: 3004,
+    },
+    preview: {
+      port,
+    },
+  }
 })
