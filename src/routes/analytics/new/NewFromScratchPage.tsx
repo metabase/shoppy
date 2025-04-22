4dd9aafda68b542b4f7eec29b6c1401a28bfb844
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-04-22 20:42:37 +0700

    Add Metabot to shoppy

--- src/routes/analytics/new/NewFromScratchPage.tsx
+++ src/routes/analytics/new/NewFromScratchPage.tsx
@@ -1,31 +1,10 @@
 import { Container } from "@mantine/core"
-import {
-  InteractiveQuestion,
-  MetabotQuestion,
-} from "@metabase/embedding-sdk-react"
-
-import { useCreateQuestionHelpers } from "../../../utils/use-create-question-helpers"
-import { useSearchParams } from "wouter"
-import { DATA_PICKER_ALLOWED_ENTITY_TYPES } from "../../../constants/data-picker"
+import { MetabotQuestion } from "@metabase/embedding-sdk-react"
 
 export const NewFromScratchPage = () => {
-  const { collectionId, onSaveQuestion } = useCreateQuestionHelpers()
-  const [searchParams] = useSearchParams()
-  const shouldShowMetabot = searchParams.get("metabot") === "true"
-
   return (
     <Container w="100%">
-      {shouldShowMetabot ? (
-        <MetabotQuestion />
-      ) : (
-        <InteractiveQuestion
-          questionId="new"
-          onSave={onSaveQuestion}
-          targetCollection={collectionId}
-          entityTypes={DATA_PICKER_ALLOWED_ENTITY_TYPES}
-          isSaveEnabled
-        />
-      )}
+      <MetabotQuestion />
     </Container>
   )
 }
