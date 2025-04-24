21ff5b7a066de25a44b543200e5f8a83cbb8496d
Timofey Kachalov <timofei@metabase.com>
2025-04-24 17:56:22 +0400

    Adjust e2e to run with app db dump from s3 (#118)

--- metabase/entrypoint.sh
+++ metabase/entrypoint.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 set -e
 
+DUMP=./app/metabase_dump.sql
+
+if [ -f $DUMP ]; then
+  echo "Restoring MB App DB from dump...";
+
+  METABASE_APP_DB_URL="postgres://${MB_DB_USER}:${MB_DB_PASS}@${MB_DB_HOST}:${MB_DB_PORT}/${MB_DB_DBNAME}"
+  METABASE_APP_DB_DUMP_TYPE=$(file --brief --mime-type "$DUMP")
+
+  case "$METABASE_APP_DB_DUMP_TYPE" in
+    application/x-tar|application/octet-stream)
+      echo "Custom　or directory‑format archive → pg_restore"
+      pg_restore -d "$METABASE_APP_DB_URL" "$DUMP" --no-owner > /dev/null
+      ;;
+    text/plain)
+      echo "Plain text SQL → psql"
+      psql "$METABASE_APP_DB_URL" -f "$DUMP" --quiet
+      ;;
+    *)
+      echo "Unknown dump format..."
+      ;;
+  esac
+fi
+
 ./app/run_metabase.sh "$@" &
 SERVER_PID=$!
 
@@ -9,17 +32,21 @@ until curl -s http://metabase:${MB_JETTY_PORT}/api/health | grep -q "\"status\":
   sleep 2
 done
 
-echo "Metabase is healthy. Running import..."
+echo "Metabase is healthy. Running additional configuration..."
 
-SESSION_ID=$(curl -s -X POST \
-  -H "Content-Type: application/json" \
-  -d "{\"username\": \"${METABASE_ADMIN_EMAIL}\", \"password\": \"${METABASE_ADMIN_PASSWORD}\"}" \
-  http://metabase:${MB_JETTY_PORT}/api/session | sed -n 's/.*"id":"\([^"]*\)".*/\1/p')
+if [ ! -f ./app/metabase_dump.sql ]; then
+  echo "Running import...";
 
-curl -X POST \
-  -H "X-Metabase-Session: $SESSION_ID" \
-  -F file=@./app/metabase_data.tar.gz \
-  http://metabase:${MB_JETTY_PORT}/api/ee/serialization/import
+  SESSION_ID=$(curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -d "{\"username\": \"${METABASE_ADMIN_EMAIL}\", \"password\": \"${METABASE_ADMIN_PASSWORD}\"}" \
+    http://metabase:${MB_JETTY_PORT}/api/session | sed -n 's/.*"id":"\([^"]*\)".*/\1/p')
+
+  curl -X POST \
+    -H "X-Metabase-Session: $SESSION_ID" \
+    -F file=@./app/metabase_data.tar.gz \
+    http://metabase:${MB_JETTY_PORT}/api/ee/serialization/import
+fi
 
 echo "Import complete. Keeping the server running..."
 
