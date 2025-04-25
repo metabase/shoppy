e4fe8f57a2551504b3b8cb3068803faf8c18c082
Timofey Kachalov <timofei@metabase.com>
2025-04-25 16:58:26 +0400

    Remove `import of serialized data` fallback when running MB instance in Docker (#129)

--- metabase/entrypoint.sh
+++ metabase/entrypoint.sh
@@ -3,26 +3,24 @@ set -e
 
 DUMP=./app/metabase_dump.sql
 
-if [ -f $DUMP ]; then
-  echo "Restoring MB App DB from dump...";
+echo "Restoring MB App DB from dump...";
 
-  METABASE_APP_DB_URL="postgres://${MB_DB_USER}:${MB_DB_PASS}@${MB_DB_HOST}:${MB_DB_PORT}/${MB_DB_DBNAME}"
-  METABASE_APP_DB_DUMP_TYPE=$(file --brief --mime-type "$DUMP")
+METABASE_APP_DB_URL="postgres://${MB_DB_USER}:${MB_DB_PASS}@${MB_DB_HOST}:${MB_DB_PORT}/${MB_DB_DBNAME}"
+METABASE_APP_DB_DUMP_TYPE=$(file --brief --mime-type "$DUMP")
 
-  case "$METABASE_APP_DB_DUMP_TYPE" in
-    application/x-tar|application/octet-stream)
-      echo "Custom　or directory‑format archive → pg_restore"
-      pg_restore -d "$METABASE_APP_DB_URL" "$DUMP" --no-owner > /dev/null
-      ;;
-    text/plain)
-      echo "Plain text SQL → psql"
-      psql "$METABASE_APP_DB_URL" -f "$DUMP" --quiet
-      ;;
-    *)
-      echo "Unknown dump format..."
-      ;;
-  esac
-fi
+case "$METABASE_APP_DB_DUMP_TYPE" in
+  application/x-tar|application/octet-stream)
+    echo "Custom　or directory‑format archive → pg_restore"
+    pg_restore -d "$METABASE_APP_DB_URL" "$DUMP" --no-owner > /dev/null
+    ;;
+  text/plain)
+    echo "Plain text SQL → psql"
+    psql "$METABASE_APP_DB_URL" -f "$DUMP" --quiet
+    ;;
+  *)
+    echo "Unknown dump format..."
+    ;;
+esac
 
 ./app/run_metabase.sh "$@" &
 SERVER_PID=$!
@@ -33,21 +31,5 @@ until curl -s http://metabase:${MB_JETTY_PORT}/api/health | grep -q "\"status\":
 done
 
 echo "Metabase is healthy. Running additional configuration..."
-
-if [ ! -f ./app/metabase_dump.sql ]; then
-  echo "Running import...";
-
-  SESSION_ID=$(curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -d "{\"username\": \"${METABASE_ADMIN_EMAIL}\", \"password\": \"${METABASE_ADMIN_PASSWORD}\"}" \
-    http://metabase:${MB_JETTY_PORT}/api/session | sed -n 's/.*"id":"\([^"]*\)".*/\1/p')
-
-  curl -X POST \
-    -H "X-Metabase-Session: $SESSION_ID" \
-    -F file=@./app/metabase_data.tar.gz \
-    http://metabase:${MB_JETTY_PORT}/api/ee/serialization/import
-fi
-
-echo "Import complete. Keeping the server running..."
 
 wait $SERVER_PID
