9c54d1e809e3a4b5f09ee7b0b5c3fd94bc5545eb
Oisin Coveney <oisinthec@gmail.com>
2025-05-30 15:09:28 +0400

    Adjust config and JWT endpoint for 55 (#139)
    Co-authored-by: Timofei Kachalov <timofei@metabase.com>

--- api/src/routes/metabase-sso.ts
+++ api/src/routes/metabase-sso.ts
@@ -1,12 +1,7 @@
-import fetch from "node-fetch"
 import { Request, Response } from "express"
 
 import { signUserToken } from "../auth/sign"
-import { METABASE_INSTANCE_URL } from "../constants/env"
-import {
-  NO_USER_MESSAGE,
-  SSO_NOT_CONFIGURED_MESSAGE,
-} from "../constants/errors"
+import { NO_USER_MESSAGE } from "../constants/errors"
 import { getUserBySite } from "../utils/sites"
 
 export async function metabaseAuthHandler(req: Request, res: Response) {
@@ -16,31 +11,14 @@ export async function metabaseAuthHandler(req: Request, res: Response) {
     return res.status(400).json({ error: "site is not specified" })
   }
 
-  const user = await getUserBySite(site)
+  const user = getUserBySite(site)
 
   if (!user) {
     return res.status(401).json({ status: "error", message: NO_USER_MESSAGE })
   }
 
-  const ssoUrl = new URL("/auth/sso", METABASE_INSTANCE_URL)
-  ssoUrl.searchParams.set("jwt", signUserToken(user))
-  ssoUrl.searchParams.set("token", "true")
-
   try {
-    const response = await fetch(ssoUrl, { method: "GET" })
-
-    const text = await response.text()
-
-    if (text.includes(`SSO has not been enabled`)) {
-      return res.status(500).json({
-        status: "error",
-        message: SSO_NOT_CONFIGURED_MESSAGE,
-      })
-    }
-
-    const token = JSON.parse(text)
-
-    return res.status(200).json(token)
+    return res.status(200).json({ jwt: signUserToken(user) })
   } catch (error) {
     if (error instanceof Error) {
       res.status(401).json({
