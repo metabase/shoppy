98f391fefcfd8455438e0fa682afa74a5a71e623
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-11-25 20:30:57 +0700

    Fix body not passing along

--- api/src/metabase-sdk-bundle-proxy.ts
+++ api/src/metabase-sdk-bundle-proxy.ts
@@ -3,12 +3,14 @@ export default {
     const response = await fetch(
       "https://shoppy.coredev.metabase.com/app/embedding-sdk.js",
     )
+
+    const clonedResponse = response.clone()
     const headers = new Headers(response.headers)
     headers.delete("set-cookie")
     headers.set("Cache-Control", "max-age=0, must-revalidate")
     headers.set("CDN-Cache-Control", "public, max-age=2592000")
 
-    return new Response(response.body, {
+    return new Response(clonedResponse.body, {
       status: response.status,
       statusText: response.statusText,
       headers: headers,
