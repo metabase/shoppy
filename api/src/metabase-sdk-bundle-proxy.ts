69a0c2e1f68579966b41da95a821a87bc781eb7f
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-11-25 20:16:39 +0700

    Using function to remove `set-cookie` header

---
+++ api/src/metabase-sdk-bundle-proxy.ts
@@ -0,0 +1,17 @@
+export default {
+  async fetch(_: Request) {
+    const response = await fetch(
+      "https://shoppy.coredev.metabase.com/app/embedding-sdk.js",
+    )
+    const headers = new Headers(response.headers)
+    headers.delete("set-cookie")
+    headers.append("Cache-Control", "max-age=0, must-revalidate")
+    headers.append("CDN-Cache-Control", "public, max-age=2592000")
+
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: headers,
+    })
+  },
+}
