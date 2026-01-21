0f82ed4eacc78e36b44b919652a9adce9809c2e6
Timofei Kachalov <timofei@metabase.com>
2026-01-21 17:34:24 +0400

    Exclude synthetic.cy.spec.js from regular e2e runs

--- e2e/support/cypress.config.js
+++ e2e/support/cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     baseUrl: `http://localhost:${process.env.CLIENT_PORT}`,
     supportFile: path.resolve(path.join(__dirname, "./cypress.js")),
     specPattern: path.resolve(path.join(__dirname, "../test/**/*.cy.spec.js")),
+    excludeSpecPattern: ["**/synthetic.cy.spec.js"],
     defaultBrowser: process.env.CYPRESS_BROWSER ?? "chrome",
   },
 })
