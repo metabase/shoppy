11da8cd48504771d2e2963c452a29f7db086435b
Timofei Kachalov <timofei@metabase.com>
2026-02-12 17:44:29 +0400

    Fix specPattern

--- e2e/support/cypress.config.js
+++ e2e/support/cypress.config.js
@@ -6,10 +6,14 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: `http://localhost:${process.env.CLIENT_PORT}`,
     supportFile: path.resolve(path.join(__dirname, "./cypress.js")),
-    specPattern: path.resolve(path.join(__dirname, "../test/**/*.cy.spec.js")),
-    ...(!process.env.INCLUDE_SYNTHETIC_MONITORING_TESTS && {
-      excludeSpecPattern: ["**/synthetic.cy.spec.js"],
-    }),
+    specPattern: path.resolve(
+      path.join(
+        __dirname,
+        process.env.INCLUDE_SYNTHETIC_MONITORING_TESTS
+          ? "../test/synthetic.cy.spec.js"
+          : "../test/!(synthetic).cy.spec.js",
+      ),
+    ),
     env: { PR_NUMBER: process.env.PR_NUMBER },
     defaultBrowser: process.env.CYPRESS_BROWSER ?? "chrome",
     userAgent: "metabase-bot/1.0",
